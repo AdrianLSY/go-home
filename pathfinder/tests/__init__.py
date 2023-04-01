6d552ccfc850684097c8c283b3a459be6d8ec285
AdrianLSY <sites.ad.p3@gmail.com>
2023-04-02 02:45:46 +1000

    updated test runner

--- pathfinder/tests/__init__.py
+++ pathfinder/tests/__init__.py
@@ -1,6 +1,13 @@
 import os
 import unittest
 
-# Searches all directories inside tests/ and runs all test suites found inside the subdirectory
-for directory in [directory for directory in os.listdir(f"{os.path.dirname(os.path.abspath(__file__))}") if os.path.isdir(os.path.join(f"{os.path.dirname(os.path.abspath(__file__))}", directory)) and directory != '__pycache__']:
-    unittest.TextTestRunner(verbosity=2).run(unittest.TestLoader().discover(f"{os.path.dirname(os.path.abspath(__file__))}/{directory}"))
+
+# Initialize a TestSuite to collect all the tests
+tests = unittest.TestSuite()
+
+# Searches all directories and collects all test suites found inside the subdirectory
+for directory in [directory for directory in os.listdir(os.path.dirname(os.path.abspath(__file__))) if os.path.isdir(f"{os.path.dirname(os.path.abspath(__file__))}/{directory}") and directory != '__pycache__']:
+    tests.addTests(unittest.TestLoader().discover(f"{os.path.dirname(os.path.abspath(__file__))}/{directory}"))
+
+# Run all tests in the collected TestSuite
+unittest.TextTestRunner(verbosity=2).run(tests)
